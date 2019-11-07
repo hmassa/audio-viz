e783bc48348028e05b7fe0801844c912abbd108b
Haley Massa <hemxvc@mail.missouri.edu>
2019-11-06 21:09:18 -0600

    hopefully adding file

---
+++ AudioViz/src/audioviz/HemxvcCoolViz.java
@@ -0,0 +1,204 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package audioviz;
+
+
+import static java.lang.Integer.min;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.transform.Rotate;
+
+/**
+ *
+ * @author Haley Massa
+ */
+public class HemxvcCoolViz implements Visualizer {
+
+    private final String name = "Hemxvc Cool Viz";
+    
+    private String vizPaneInitialStyle = "";
+    
+    private AnchorPane vizPane;
+    private Integer numRows;
+    private Integer numBoxes;
+    
+    private Double width = 0.0;
+    private Double height = 0.0;
+    
+    private Label label;
+    private Circle sticker;
+    private Circle hole;
+    
+    private Double boxWidth;
+    
+    private ImageView iv;
+    private Image record;
+    private final String imageName = "HemxvcVinyl.png";
+    private final Double diameter = 340.0;
+    
+    private final Double startHue = 0.0;
+    private final Double angleChange = 10.0; // ~33.33 rpm
+    private final Double left = 45.0;
+    private final Double up = 30.0;
+    
+    private Rectangle[][] boxesL;
+    private Rectangle[][] boxesR;
+    
+    @Override
+    public void start(Integer numBands, AnchorPane vizPane) {
+        end();
+        
+        vizPaneInitialStyle = vizPane.getStyle();
+        
+        this.vizPane = vizPane;
+        numBoxes = numBands;
+        numRows = (int)Math.sqrt(numBands);
+        
+        height = vizPane.getHeight();
+        width = vizPane.getWidth();
+        
+        Rectangle clip = new Rectangle(width, height);
+        clip.setLayoutX(0);
+        clip.setLayoutY(0);
+        vizPane.setClip(clip);
+        
+        record = new Image(getClass().getResourceAsStream(imageName));
+        iv = new ImageView();
+        iv.setImage(record);
+        iv.setFitWidth(diameter);
+        iv.setFitHeight(diameter);
+        iv.setLayoutX(width/2 - iv.getFitWidth()/2);
+        iv.setLayoutY(height/2 - iv.getFitHeight()/2);
+        vizPane.getChildren().add(iv);
+        
+        sticker = new Circle(width/2, height/2, 55.0);
+        sticker.setFill(Color.hsb(startHue, 1.0, 1.0, 1.0));
+        vizPane.getChildren().add(sticker);
+        
+        hole = new Circle(width/2, height/2, 5.0);
+        hole.setFill(Color.BLACK);
+        vizPane.getChildren().add(hole);
+        
+        label = new Label(name);
+        label.setTextFill(Color.WHITE);
+        label.setLayoutX(width/2 - left);
+        label.setLayoutY(height/2 - up);
+        vizPane.getChildren().add(label);
+        
+        boxesL = new Rectangle[numRows][numRows];
+        Double displayWidth = (width - diameter)/2 - 10;
+        boxWidth = displayWidth/numRows - 3;
+        Double yOffset = (height-displayWidth)/2;
+        
+        for(int i = 0; i < numRows; i++){
+            for(int j = 0; j < numRows; j++){
+                Rectangle box = new Rectangle(boxWidth, boxWidth);
+                box.setX(j*(boxWidth + 3) + 5);
+                box.setY(i*(boxWidth + 3) + yOffset);
+                box.setFill(Color.BLACK);
+                boxesL[i][j] = box;
+                vizPane.getChildren().add(box);
+            }  
+        }
+        
+        boxesR = new Rectangle[numRows][numRows];
+        Double xOffset = width - displayWidth - 5;
+        
+        for(int i = 0; i < numRows; i++){
+            for(int j = 0; j < numRows; j++){
+                Rectangle box = new Rectangle(boxWidth, boxWidth);
+                box.setX(j*(boxWidth + 3) + xOffset);
+                box.setY(i*(boxWidth + 3) + yOffset);
+                box.setFill(Color.BLACK);
+                boxesR[i][j] = box;
+                vizPane.getChildren().add(box);
+            }  
+        }
+    }
+
+    @Override
+    public void end() {
+        if (boxesL != null){
+            for(Rectangle[] bL : boxesL){
+                for(Rectangle box : bL){
+                    vizPane.getChildren().remove(box);
+                }
+            }
+        }
+        if (boxesR != null){
+            for(Rectangle[] bR : boxesR){
+                for(Rectangle box : bR){
+                    vizPane.getChildren().remove(box);
+                }
+            }
+        }
+        if (label != null){
+            vizPane.getChildren().remove(label);
+        }
+        if (iv != null) {
+            vizPane.getChildren().remove(iv);
+        }
+        if (sticker != null) {
+            vizPane.getChildren().remove(sticker);
+        }
+        if (hole != null) {
+            vizPane.getChildren().remove(hole);
+            vizPane.setClip(null);
+            vizPane.setStyle(vizPaneInitialStyle);
+        } 
+    }
+
+    @Override
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public void draw(double timestamp, double length, float[] magnitudes, float[] phases) {
+        if (label == null) {
+            return;
+        }
+        Double hue = magnitudes[0]*-6.0;
+        sticker.setFill(Color.hsb(hue, 1.0, 1.0, 1.0));
+        
+        label.getTransforms().add(new Rotate(angleChange, left, up));
+        iv.setRotate((200.0*timestamp)%360); // ~33.33 rpm
+        
+        Integer m = min(magnitudes.length, numBoxes);
+        Integer p = min(phases.length, numBoxes);
+        
+        for(int i = 0; i < m; i++){
+            int col = (int)i/numRows;
+            int row = i%numRows;
+            if((((magnitudes[i]*-6.0)%360)/360) > 0){
+                boxesL[numRows-1-row][col].setFill(Color.hsb(magnitudes[i]*-6.0, 1.0, 1.0));
+                boxesR[numRows-1-row][numRows-1-col].setFill(Color.hsb(magnitudes[i]*-6.0, 1.0, 1.0));
+            }
+            else {
+                boxesL[numRows-1-row][col].setFill(Color.BLACK);
+                boxesR[numRows-1-row][numRows-1-col].setFill(Color.BLACK);
+            }
+        }
+        for(int j=0; j < p; j++){
+            int row = j/numRows;
+            int col = j%numRows;
+            {
+                boxesL[row][col].setHeight(boxWidth + phases[j]);
+                boxesL[row][col].setWidth(boxWidth + phases[j]);
+                boxesR[row][col].setHeight(boxWidth + phases[j]);
+                boxesR[row][col].setWidth(boxWidth + phases[j]);
+            }
+        }
+        
+//        vizPane.setStyle("-fx-background-color: hsb(" + magnitudes[0]*-6.0 + 
+//                ", 10%, 100%)" );
+    }
+}
---
+++ AudioViz/src/audioviz/PlayerController.java
@@ -0,0 +1,281 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package audioviz;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.MediaView;
+import javafx.scene.text.Text;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+/**
+ * FXML Controller class
+ *
+ * @author Professor Wergeles 
+ * 
+ * Music: http://www.bensound.com/royalty-free-music
+ * http://www.audiocheck.net/testtones_sinesweep20-20k.php
+ * http://stackoverflow.com/questions/11994366/how-to-reference-primarystage
+ */
+public class PlayerController implements Initializable {
+
+    @FXML
+    private AnchorPane vizPane;
+
+    @FXML
+    private MediaView mediaView;
+
+    @FXML
+    private Text filePathText;
+
+    @FXML
+    private Text lengthText;
+
+    @FXML
+    private Text currentText;
+
+    @FXML
+    private Text bandsText;
+
+    @FXML
+    private Text visualizerNameText;
+
+    @FXML
+    private Text errorText;
+
+    @FXML
+    private Menu visualizersMenu;
+
+    @FXML
+    private Menu bandsMenu;
+
+    @FXML
+    private Slider timeSlider;
+
+    @FXML
+    private Button playPause;
+
+    private Media media;
+    private MediaPlayer mediaPlayer;
+
+    private Integer numOfBands = 36;
+    private final Double updateInterval = 0.05;
+
+    private ArrayList<Visualizer> visualizers;
+    private Visualizer currentVisualizer;
+    private final Integer[] bandsList = {1, 4, 9, 16, 25, 36, 49, 64, 81, 100};
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        bandsText.setText(Integer.toString(numOfBands));
+
+        visualizers = new ArrayList<>();
+        visualizers.add(new EllipseVisualizer1());
+        visualizers.add(new EllipseVisualizer2());
+        visualizers.add(new EllipseVisualizer3());
+        visualizers.add(new HemxvcCoolViz());
+        for (Visualizer visualizer : visualizers) {
+            MenuItem menuItem = new MenuItem(visualizer.getName());
+            menuItem.setUserData(visualizer);
+            menuItem.setOnAction((ActionEvent event) -> {
+                selectVisualizer(event);
+            });
+            visualizersMenu.getItems().add(menuItem);
+        }
+        currentVisualizer = visualizers.get(3);
+        visualizerNameText.setText(currentVisualizer.getName());
+
+        for (Integer bands : bandsList) {
+            MenuItem menuItem = new MenuItem(Integer.toString(bands));
+            menuItem.setUserData(bands);
+            menuItem.setOnAction((ActionEvent event) -> {
+                selectBands(event);
+            });
+            bandsMenu.getItems().add(menuItem);
+        }
+    }
+
+    private void selectVisualizer(ActionEvent event) {
+        MenuItem menuItem = (MenuItem) event.getSource();
+        Visualizer visualizer = (Visualizer) menuItem.getUserData();
+        changeVisualizer(visualizer);
+    }
+
+    private void selectBands(ActionEvent event) {
+        MenuItem menuItem = (MenuItem) event.getSource();
+        numOfBands = (Integer) menuItem.getUserData();
+        if (currentVisualizer != null) {
+            currentVisualizer.start(numOfBands, vizPane);
+        }
+        if (mediaPlayer != null) {
+            mediaPlayer.setAudioSpectrumNumBands(numOfBands);
+        }
+        bandsText.setText(Integer.toString(numOfBands));
+    }
+
+    private void changeVisualizer(Visualizer visualizer) {
+        if (currentVisualizer != null) {
+            currentVisualizer.end();
+        }
+        currentVisualizer = visualizer;
+        currentVisualizer.start(numOfBands, vizPane);
+        visualizerNameText.setText(currentVisualizer.getName());
+    }
+
+    private void openMedia(File file) {
+        filePathText.setText("");
+        errorText.setText("");
+
+        if (mediaPlayer != null) {
+            mediaPlayer.dispose();
+        }
+
+        try {
+            media = new Media(file.toURI().toString());
+            mediaPlayer = new MediaPlayer(media);
+            mediaView.setMediaPlayer(mediaPlayer);
+            mediaPlayer.setOnReady(() -> {
+                handleReady();
+            });
+            mediaPlayer.setOnEndOfMedia(() -> {
+                handleEndOfMedia();
+            });
+            mediaPlayer.setAudioSpectrumNumBands(numOfBands);
+            mediaPlayer.setAudioSpectrumInterval(updateInterval);
+            mediaPlayer.setAudioSpectrumListener((double timestamp, double duration, float[] magnitudes, float[] phases) -> {
+                handleVisualize(timestamp, duration, magnitudes, phases);
+            });
+            mediaPlayer.setAutoPlay(true);
+            filePathText.setText(file.getPath());
+        } catch (Exception ex) {
+            errorText.setText(ex.toString());
+        }
+    }
+
+    private void handleReady() {
+        Duration duration = mediaPlayer.getTotalDuration();
+        lengthText.setText(duration.toString());
+        Duration ct = mediaPlayer.getCurrentTime();
+        currentText.setText(ct.toString());
+        currentVisualizer.start(numOfBands, vizPane);
+        timeSlider.setMin(0);
+        timeSlider.setMax(duration.toMillis());
+    }
+
+    private void handleEndOfMedia() {
+        mediaPlayer.stop();
+        mediaPlayer.seek(Duration.ZERO);
+        timeSlider.setValue(0);
+    }
+
+    private void handleVisualize(double timestamp, double duration, float[] magnitudes, float[] phases) {
+        Duration ct = mediaPlayer.getCurrentTime();
+        double ms = ct.toMillis();
+        currentText.setText(String.format("%.1f ms", ms));
+        timeSlider.setValue(ms);
+
+        currentVisualizer.draw(timestamp, duration, magnitudes, phases);
+    }
+
+    @FXML
+    private void handleOpen(Event event) {
+        Stage primaryStage = (Stage) vizPane.getScene().getWindow();
+        FileChooser fileChooser = new FileChooser();
+        File file = fileChooser.showOpenDialog(primaryStage);
+        if (file != null) {
+            openMedia(file);
+        }
+    }
+
+    @FXML
+    private void handlePlayPause(ActionEvent event) {
+        if (mediaPlayer != null) {
+            if (!isPlaying()) {
+                play(); 
+            } else {
+                pause(); 
+            }
+        }
+        else {
+//            Alert alert = new Alert(AlertType.INFORMATION);
+//            alert.setTitle("Information Dialog");
+//            alert.setHeaderText("There was an error playing the song.");
+//            alert.setContentText("You must select a song first before I can play it for you.");
+//
+//            alert.showAndWait();
+        
+            File file = new File("/Users/flipp/Music/band_of_horses-everything_all_the_time-320k/band_of_horses-everything_all_the_time-320k/sp0690-04_the_funeral.mp3");
+            openMedia(file);
+        }
+
+    }
+    
+    public void play(){
+        if(mediaPlayer != null){
+            mediaPlayer.play(); 
+            playPause.setText("Pause");
+        }   
+    }
+    
+    public void pause(){
+        if(mediaPlayer != null){
+            mediaPlayer.pause();
+            playPause.setText("Play");
+        }
+    }
+    
+    public void stop(){
+        if(mediaPlayer != null){
+            mediaPlayer.stop();
+            playPause.setText("Play"); 
+        }
+    }
+
+    @FXML
+    private void handleStop(ActionEvent event) {
+        stop(); 
+    }
+
+    @FXML
+    private void handleSliderMousePressed(Event event) {
+        pause(); 
+    }
+
+    @FXML
+    private void handleSliderMouseReleased(Event event) {
+        if (mediaPlayer != null) {
+            mediaPlayer.seek(new Duration(timeSlider.getValue()));
+            System.out.println(timeSlider.getValue());
+            currentVisualizer.start(numOfBands, vizPane);
+            play(); 
+        }
+    }
+    public boolean isPlaying(){
+        if(mediaPlayer != null){
+            if(mediaPlayer.getStatus() == MediaPlayer.Status.PLAYING){
+                return true; 
+            }
+        }
+        return false; 
+    }
+}
